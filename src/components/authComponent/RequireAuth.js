dde99f1fabcc34f96b032323ef0b61f3576640f3
developers-codz <kaj123al2002@gmail.com>
2022-05-17 20:28:00 +0530

    fix:remove console

--- src/components/authComponent/RequireAuth.js
+++ src/components/authComponent/RequireAuth.js
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 
 export const RequireAuth = () => {
     const currentUser = useSelector((state)=>state.auth.currentUser)
-    console.log(currentUser._id)
     const location = useLocation();
   return currentUser._id ?<Outlet />: (<Navigate to="/login" state={{from: location}} replace />)
 }
